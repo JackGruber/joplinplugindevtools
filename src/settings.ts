183cffbc1218833fb833500c3214d9856afa911a
JackGruber <24863925+JackGruber@users.noreply.github.com>
2021-08-28 10:13:17 +0200

    fix path

--- src/settings.ts
+++ src/settings.ts
@@ -7,7 +7,7 @@ export const manifestFile = path.resolve(
   path.join(__dirname, "..", "..", "..", "src", "manifest.json")
 );
 export const packageFile = path.resolve(
-  path.join(__dirname, "..", "..", "..", "src", "package.json")
+  path.join(__dirname, "..", "..", "..", "package.json")
 );
 export const publishDir = path.resolve(
   path.join(__dirname, "..", "..", "..", "publish")
