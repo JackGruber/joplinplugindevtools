107ba34d69cd2335a95ff58648c814a24e4ed252
JackGruber <24863925+JackGruber@users.noreply.github.com>
2021-08-28 09:40:06 +0200

    correct path

--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,13 @@ import * as moment from "moment";
 import * as path from "path";
 import { execCommand } from "./execCommand";
 
+const changelogFile = path.resolve(
+  path.join(__dirname, "..", "..", "..", "CHANGELOG.md")
+);
+const manifestFile = path.resolve(
+  path.join(__dirname, "..", "..", "..", "src", "manifest.json")
+);
+
 export function readline(question: string) {
   return new Promise((resolve) => {
     const readline = require("readline");
@@ -20,29 +27,21 @@ export function readline(question: string) {
 }
 
 export async function getJPLFileName() {
-  const manifestFile = path.resolve(
-    path.join(__dirname, "..", "src", "manifest.json")
-  );
   const manifest = require(manifestFile);
   return manifest.id + ".jpl";
 }
 
 export async function updateChangelog(version: string, preRelease: boolean) {
-  const changelog = path.resolve(path.join(__dirname, "..", "CHANGELOG.md"));
-
-  let data = fs.readFileSync(changelog, { encoding: "utf8", flag: "r" });
+  let data = fs.readFileSync(changelogFile, { encoding: "utf8", flag: "r" });
   version = `${version} ${preRelease ? "[pre-release] " : ""}(${moment().format(
     "YYYY-MM-DD"
   )})`;
   data = data.replace(/## not released/, `## not released\n\n## v${version}`);
-  fs.writeFileSync(changelog, data);
+  fs.writeFileSync(changelogFile, data);
 }
 
 export async function setPluginVersion(version: string) {
   console.log("set Joplin plugin version");
-  const manifestFile = path.resolve(
-    path.join(__dirname, "../src/manifest.json")
-  );
   const manifest = require(manifestFile);
   manifest.version = version;
   fs.writeFileSync(manifestFile, JSON.stringify(manifest, null, 2));
@@ -57,8 +56,7 @@ export async function runNpmVersion(type: string) {
 }
 
 export async function getChangelog(version: string): Promise<string> {
-  const changelog = path.resolve(path.join(__dirname, "..", "CHANGELOG.md"));
-  let data = fs.readFileSync(changelog, { encoding: "utf8", flag: "r" });
+  let data = fs.readFileSync(changelogFile, { encoding: "utf8", flag: "r" });
 
   version = version.replace(/\./g, "\\.");
   const regExp = new RegExp(`(?<ENTRY>## v${version}(.|\n)*?)^##`, "im");
